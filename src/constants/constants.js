9495a3d193bddd731136960abb2656c1e5a7ff47
MAHANTESHBNM <mahanteshbnm26@gmail.com>
2023-02-11 18:07:15 +0530

    done

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,5 @@
-export const BASE_URL = "https://dword-endoxous-backend.onrender.com/api/v1";
+// export const BASE_URL = "https://dword-endoxous-backend.onrender.com/api/v1";
 //export const BASE_URL = "http://localhost:4000/api/v1" ;
+export const BASE_URL = "https://endoxous-server.onrender.com/api/v1";
+
+
